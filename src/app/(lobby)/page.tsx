c8532656745d04c741a1e7f2a94434c6c70dfe39
d4t06 <huudat01234560@gmail.com>
2024-02-29 11:15:17 +0700

    update thu 6 1/3/2024

---
+++ src/app/(lobby)/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+// import { useSession } from "next-auth/react";
+
+export default function HomePage() {
+   // const { data, status } = useSession();
+
+   return <p>This is home page</p>;
+}
